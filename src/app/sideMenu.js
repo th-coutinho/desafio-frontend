689eebd48f2b5808af0a4fb797c4253b39fca3df
Thiago Coutinho <thiagoscoutinho92@gmail.com>
2019-06-02 20:23:36 -0300

    Feature: Blur Effect and outside click closes menu

--- src/app/sideMenu.js
+++ src/app/sideMenu.js
@@ -1,6 +1,30 @@
-const toggleMobileMenu = () => {
-    document.querySelector(".side-menu").classList.toggle('visible');
-    document.querySelector(".side-menu .side-menu-logo").classList.toggle('show');
+let sideMenuIsOpen = false;
+
+const blurDivs = () => {
+    const divs = [
+        '.main',
+        '.about-me',
+        '.challenge',
+        '.footer'
+    ]
+
+    divs.forEach(selector => {
+        document.querySelector(selector).classList.toggle('blur');
+    });
 }
 
-export { toggleMobileMenu }
+const closeMenuOnOutsideClick = () => {
+    if (sideMenuIsOpen) {
+        toggleMobileMenu();
+    }
+}
+
+const toggleMobileMenu = () => {
+    sideMenuIsOpen = !sideMenuIsOpen;
+    document.querySelector(".side-menu").classList.toggle('visible');
+    document.querySelector(".side-menu-shadow").classList.toggle('visible');
+    document.querySelector(".side-menu .side-menu-logo").classList.toggle('show');
+    blurDivs();
+}
+
+export { toggleMobileMenu, closeMenuOnOutsideClick }
